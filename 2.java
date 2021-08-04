66d09dea2f767768071c2a777f816ac88b89c940
marvelo379 <84303180+marvelo379@users.noreply.github.com>
2021-08-04 19:48:51 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,48 @@
+************************************************JAVA TUTORIAL*********************************************************************************************************
+
+
+1.INT TO STR:
+    int n=10;
+    String num=String.valueof(n);
+    num
+    
+     # String num=Integer.toString(n);
+2. STR TO LONG
+   String num=10;
+   long n=Long.parseLong(num);
+   n
+3.LONG TO STR:
+       long num=102536;
+       String n=Long.toString(num);
+ # String n=String.valueof(num)
+       num
+4. string to float:
+        String n="125.5"
+        float num=float.parseFloat(n)
+        n
+5. string to double/double to str
+6. string to char:
+    * String name="mankind"
+     char c=name.charAt(0);
+   * for(int i=0;i<name.length;i++){
+              char c=name.charAt(i);
+              sopln(c);
+     } 
+    * String num="mankind"
+      char[] ch=num.toCharArray();
+      for(int i=0;i<num.length;i++){
+               sopln(ch[i]);
+       }
+       sopln(ch);
+7.char to str:
+     char c="a";
+     String ns=String.valueof(c)
+     ns+"ram";
+     #Character.toString(c)
+8. int to long
+   int x=10;
+   long n=x;
+9. long to int :
+           long s=1022245406
+           
+     
